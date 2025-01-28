d3e61fc2df3657fe6a882ba6ffa40c97ba2c0fa2
Mahendra Paipuri <44365948+mahendrapaipuri@users.noreply.github.com>
2025-01-28 10:07:05 +0100

    fix: MarshalYAML receivers on `TLSVersion` and `Curve`
    * Currently the `MarshalYAML` receivers are defined on pointers of `TLSVersion` and `Curve` but `TLSConfig` is defined with values of `TLSVersion` and `Curve` in its fields. So, `MarshalYAML` receiver is never called upon. This commit fixes it by changing receivers on values which should work on both values and pointers.
    
    Signed-off-by: Mahendra Paipuri <44365948+mahendrapaipuri@users.noreply.github.com>

--- web/tls_config.go
+++ web/tls_config.go
@@ -481,7 +481,7 @@ func (c *Curve) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return errors.New("unknown curve: " + s)
 }
 
-func (c *Curve) MarshalYAML() (interface{}, error) {
+func (c Curve) MarshalYAML() (interface{}, error) {
 	for s, curveid := range curves {
 		if *c == curveid {
 			return s, nil
@@ -512,7 +512,7 @@ func (tv *TLSVersion) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	return errors.New("unknown TLS version: " + s)
 }
 
-func (tv *TLSVersion) MarshalYAML() (interface{}, error) {
+func (tv TLSVersion) MarshalYAML() (interface{}, error) {
 	for s, v := range tlsVersions {
 		if *tv == v {
 			return s, nil
